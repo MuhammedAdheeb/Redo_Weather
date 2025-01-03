9e89347b44cb04b50df05bbd6be0e469a126e569
MuhammedAdheeb <azahim.20210443@iit.ac.lk>
2025-01-03 09:19:08 +0800

    Initial Commit

---
+++ Redo_Weather/Redo_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redo_WeatherApp.swift
+//  Redo_Weather
+//
+//  Created by user271431 on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Redo_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
